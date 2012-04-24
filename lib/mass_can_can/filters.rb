7cf6b69858c81cfba2f907e947323edc9e87e045
PoTe <pote@tardis.com.uy>
2012-04-24 14:24:33 -0400

    delete commented code

--- lib/mass_can_can/filters.rb
+++ lib/mass_can_can/filters.rb
@@ -3,7 +3,6 @@ module MassCanCan
     extend ActiveSupport::Concern
 
     included do
-    #around_filter :set_authorizer
     before_filter :initialize_mass_can_can
     end
 
@@ -11,13 +10,5 @@ module MassCanCan
     #MassCanCan::Ability.initialize_module
     current_ability
     end
-
-    #  #def set_authorizer
-    #  #  Thread.current[:_current_authorizer] = MassCanCan::Authorizer.new(current_ability)
-    #  #  auth, ActiveRecord::Base.active_authorizer = ActiveRecord::Base.active_authorizer, Thread.current[:_current_authorizer]
-    #  #  yield
-    #  #ensure
-    #  #  ActiveRecord::Base.active_authorizer = auth
-    #  #end
   end
 end
--- lib/tasks/mass_can_can_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :mass_can_can do
-#   # Task goes here
-# end
